b27cb463ff8572f200afd3c81e11424838453ebc
PhABC <ph.castonguay@gmail.com>
2021-11-17 12:11:41 -0500

    Fix mock contracts compiling

--- src/contracts/mocks/ERC1155Mock.sol
+++ src/contracts/mocks/ERC1155Mock.sol
@@ -5,5 +5,5 @@ import "@0xsequence/erc-1155/contracts/mocks/ERC1155MintBurnMock.sol";
 
 
 contract ERC1155Mock is ERC1155MintBurnMock {
-
+  constructor() ERC1155MintBurnMock("TestERC1155", "") {}
 }
