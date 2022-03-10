fb5b76337e7b04c05bce128e46568879bee5e51f
Agustin Aguilar <agusgit@pm.me>
2022-03-10 15:41:40 +0000

    Increase optimizer runs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,7 +11,7 @@ const config: HardhatUserConfig = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 825,
+        runs: 833,
         details: {
           yul: true
         }
