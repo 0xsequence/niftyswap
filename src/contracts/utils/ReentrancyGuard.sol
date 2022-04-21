21caee9cce6492c3bba5416bca97e4c07855b79d
PhABC <ph.castonguay@gmail.com>
2022-04-21 13:26:58 -0400

    Resolve issue #4

--- src/contracts/utils/ReentrancyGuard.sol
+++ src/contracts/utils/ReentrancyGuard.sol
@@ -1,4 +1,6 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (security/ReentrancyGuard.sol)
+
 pragma solidity 0.7.4;
 
 /**
@@ -13,40 +15,49 @@ pragma solidity 0.7.4;
  * those functions `private`, and then adding `external` `nonReentrant` entry
  * points to them.
  *
- * _Since v2.5.0:_ this module is now much more gas efficient, given net gas
- * metering changes introduced in the Istanbul hardfork.
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
  */
-contract ReentrancyGuard {
-  bool private _notEntered;
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
 
-  constructor () {
-    // Storing an initial non-zero value makes deployment a bit more
-    // expensive, but in exchange the refund on every call to nonReentrant
-    // will be lower in amount. Since refunds are capped to a percetange of
-    // the total transaction's gas, it is best to keep them low in cases
-    // like this one, to increase the likelihood of the full refund coming
-    // into effect.
-    _notEntered = true;
-  }
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
 
-  /**
-   * @dev Prevents a contract from calling itself, directly or indirectly.
-   * Calling a `nonReentrant` function from another `nonReentrant`
-   * function is not supported. It is possible to prevent this from happening
-   * by making the `nonReentrant` function external, and make it call a
-   * `private` function that does the actual work.
-   */
-  modifier nonReentrant() {
-    // On the first call to nonReentrant, _notEntered will be true
-    require(_notEntered, "RG#1"); // ReentrancyGuard: reentrant call
+    uint256 private _status;
 
-    // Any calls to nonReentrant after this point will fail
-    _notEntered = false;
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
 
-    _;
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and making it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
 
-    // By storing the original value once again, a refund is triggered (see
-    // https://eips.ethereum.org/EIPS/eip-2200)
-    _notEntered = true;
-  }
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
 }
